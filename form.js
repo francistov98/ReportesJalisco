120d47ebe2d33860d741f588eec9fd9201614728
francistov98 <60896967+francistov98@users.noreply.github.com>
2020-11-30 19:51:18 -0600

    Update form.js

--- form.js
+++ form.js
@@ -19,34 +19,40 @@ var inputs = document.getElementsByClassName('formulario__input');
  
 (function ()
 {
-	var output = document.getElementById('map');
+	const output = document.getElementById('map');
 	if (navigator.geolocation)
 	{
 		output.innerHTML = "<p> Tu navegador soporta geolocalizacion</p>";
-		var map;
-		var mapOptions = 
+		const mapOptions = 
 		{
 			zoom:20,
 			mapTypeId: google.maps.MapTypeId.ROAD
 		};
-		map = new google.maps.Map(document.getElementById("map"),mapOptions);
+		const map = new google.maps.Map(document.getElementById("map"),mapOptions);
 		navigator.geolocation.getCurrentPosition
 		(
 			function(position)
 			{
-				var latitude = position.coords.latitude;
-				var longitude = position.coords.longitude;
-				var geolocate = new google.maps.LatLng(latitude,longitude);
-				var infoWindow = new google.maps.InfoWindow
+				const pos = {lat: position.coords.latitude, lng: position.coords.longitude};
+				const geolocate = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+				const contenidoInfo = '<h1>Esta es tu ubicacion</h1>'+
+									'<h2>Latitud: '+position.coords.latitude+'</h2>'+
+									'<h2>Longitud: '+position.coords.longitude+'</h2>';
+				const infowindow = new google.maps.InfoWindow
 				(
 					{
-						map: map,
-						position: geolocate,
-						content:'<h1>Esta es tu ubicacion</h1>' +
-								'<h2>Latitud: '+latitude+'</h2>' +
-								'<h2>Longitud: '+longitude+'</h2>'
+						content: contenidoInfo,
 					}
-				); 
+				);
+				const marker = new google.maps.Marker({
+					position: geolocate,
+					map,
+					title: "Tu ubicacion actual",
+				  });
+				 marker.addListener("click", () => {
+					infowindow.open(map, marker);
+				  });
+
 				map.setCenter(geolocate);
 			}
 		);
@@ -61,10 +67,10 @@ var inputs = document.getElementsByClassName('formulario__input');
 		//var imgURL = "https://maps.googleapis.com/maps/api/staticmap?center="+latitude+","+longitude+"&size=600x300&markers=color:red%7C"+latitude+","+longitude+"&key=AIzaSyAKNXDvttVTGjyVAutQH3TOXIiCFcbXyg8";
 		//output.innerHTML = "<p>Latitud: "+latitude+"<br>Longitud: "+longitude+"</p>"
 		//output.innerHTML = "<img src= '"+imgURL+"'>";
-	}*/
+	}
 	function error()
 	{
 		output.innerHTML = "<p>No se pudo obtener tu geolocalizacion</p>";
 	}
-	//navigator.geolocation.getCurrentPosition(localizacion, error);
+	//navigator.geolocation.getCurrentPosition(localizacion, error);*/
 })(); 
