0b7c743a7b65654e144f071e28a386611526bcd9
francistov98 <60896967+francistov98@users.noreply.github.com>
2020-12-02 16:11:47 -0600

    Delete conexionDB.php

--- conexionDB.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conex = pg_connect("user=tiftlrnxzklzil password=70e789b4bba586c858706486f01284b534007bb3d3a816f70afad94916b39f7c host=ec2-34-204-121-199.compute-1.amazonaws.com dbname=d9qga0jot4pjoa") or die( "Error al conectar: ".pg_last_error() );
-?>
