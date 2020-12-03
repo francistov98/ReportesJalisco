a05d5f86c5d41758392ec4febcaf1abd8fed86db
francistov98 <60896967+francistov98@users.noreply.github.com>
2020-12-03 12:51:15 -0600

    Update administrador.php

--- administrador.php
+++ administrador.php
@@ -9,12 +9,20 @@ include('sesion.php');
 		<link rel="stylesheet" href="estilos.css">
 	</head>
 	<body>
-	<h3>Bienvenid@ al sistema  <i><?php echo $login_session; ?></i></h3>
+	<h1>Bienvenid@ al sistema  <i><?php echo $login_session; ?></i></h1>
 	<div class="clear"> </div>
 	<h4><a href="logout.php"> Cerrar sesión</a></h4>
 	</div>	
+	<div>
+		<form method="post">
+			<p>Ingresa el campo que deseas buscar en la base de datos: <input type="text" name="consulta"></p>
+			<input type="submit" name="submit" value="Buscar">
+		</form>
 		<?php
-			include('mostrarFormularios.php');
+			include("busquedaEspecifica.php");
 		?>
+	</div>
+	<div>
+	</div>
 	</body>
 </html>
